4ae9e112ab76ff231a0e9758e5caf780fc879c33
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-17 15:43:28 -0400

    3/17/2016

--- homework/assets/javascript/file.js
+++ homework/assets/javascript/file.js
@@ -1,33 +1,22 @@
 $(document).ready(function() {
-
 	var database = new Firebase("https://howfayatrain.firebaseio.com/");
 
 	
 	$('#submit').on('click', function() {
-	var trainId = $('#train-name').val().trim();
+	var TrainId = $('#train-name').val().trim();
 	var destination = $('#destination').val().trim();
-	var arrival = $('#first-train').val().trim();
+	var firstTrain = $('#first-train').val().trim();
 	var frequency = $('#frequency').val().trim();
-
-		
-		database.push ({
-		name: trainId,
-		destination: destination,
-		frequency: frequency,
-		firstTime: firstTimeConverted,
-		minutesAway: tMinutesTillTrain,
-		nextArrival: arrival
-		});
-});	
+	
 	// First Time (pushed back 1 year to make sure it comes before current time)
-	var firstTimeConverted = moment(arrival,"hh:mm").subtract(1, "years");
-		// console.log(firstTimeConverted);
+	var ArrivalConverted = moment(firstTrain,"hh:mm").subtract(1, "years");
+		// console.log(ArrivalConverted);
 
 	// Current Time
 	var currentTime = moment();
 		// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	var diffTime = moment().diff(moment(ArrivalConverted), "minutes");
 		// console.log("DIFFERENCE IN TIME: " + diffTime);
 
 	// Time apart (remainder)
@@ -44,6 +33,16 @@ $(document).ready(function() {
 
 	var arrival = moment(nextTrain).format("hh:mm");
 
+		
+		database.push({
+		name: TrainId,
+		destination: destination,
+		frequency: frequency,
+		Arrival: ArrivalConverted,
+		Minutes: tMinutesTillTrain,
+		Arrival: arrival
+		});
+});	
 	setInterval(date,1000);
 	function date()	{
 		$("#date").html(moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'));
@@ -52,11 +51,9 @@ $(document).ready(function() {
 
 	database.on('child_added', function(snapshot) {
 
-
 		$(".table > tbody").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination +
-		 "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().nextArrival + "</td><td>" + 
-		 snapshot.val().minutesAway + "</td></tr>");
+		 "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().Arrival + "</td><td>" + 
+		 snapshot.val().Minutes + "</td></tr>");
 		});
-
 	
-}); 
+});
